68d95e2167e9ce2bff25b25ba018506d47ab2a43
CaptainAlcoby <75146077+CaptainAlcoby@users.noreply.github.com>
2021-03-16 20:37:21 +0000

    Update input.c

--- input.c
+++ input.c
@@ -11,16 +11,16 @@
 
 void helpMenu(void)
 {
-	printf("Commands: \n");
-	printf("join id \n"); //Entrada de um nó na rede com identificador id
-	printf("join id bootIP bootTCP\n"); //Entrada de um nó na rede com identificador id. É passado o contacto de um nó da rede, através dos parâmetros bootIP e bootTCP, ao qual o nó se deverá ligar sem interrogar o servidor de nós
+	printf("\nCommands:\n");
+	printf("join net id \n"); //Entrada de um nó na rede net com identificador id
+	printf("join net id bootIP bootTCP\n"); //Entrada de um nó na rede net com identificador id. É passado o contacto de um nó da rede, através dos parâmetros bootIP e bootTCP, ao qual o nó se deverá ligar sem interrogar o servidor de nós
 	printf("create subname\n"); //É criado um objeto cujo nome será da forma id.subname, em que id é o identificador do nó
 	printf("get name\n"); //Inicia-se a pesquisa do objeto com o nome name. Este nome será da forma id.subname, em que id é o identificador de um nó e subname é o sub-nome do objeto atribuído pelo nó com identificador id
 	printf("show topology (st)\n"); //Mostra os contactos do vizinho externo e do vizinho de recuperação
 	printf("show routing (sr)\n"); //Mostra a tabela de expedição do nó
 	printf("show cache (sc)\n"); //Mostra os nomes dos objetos guardados na cache
 	printf("leave\n"); //Saída do nó da rede
-	printf("exit\n"); //Fecho da aplicação
+	printf("exit\n\n"); //Fecho da aplicação
 }
 
 char* getParam(char *input)
@@ -39,39 +39,27 @@ char* getParam(char *input)
 
 char *readCommand(enum instr *instr_code)
 {
-	char terminal[128], command[14], bootIP[INET_ADDRSTRLEN], bootTCP[NI_MAXSERV], lixo[2], second[20];
+	char terminal[128], command[14], bootIP[INET_ADDRSTRLEN], bootTCP[NI_MAXSERV], second[64], id[64];
 	int size_input = 0;
 	
 	fgets(terminal,128,stdin);
-	size_input = sscanf(terminal,"%s %s %s %s %s", command, second, bootIP, bootTCP, lixo);
+	size_input = sscanf(terminal,"%s %s %s %s %s", command, second, id, bootIP, bootTCP);
 	*instr_code = ERR;
-
-	if(size_input <= 0)
-	{
-		printf("Error in input\n");
-		return NULL;
-	}
-	
-	if(size_input == 5)
-	{
-		printf("Invalid command\n");
-		return NULL;
-	}
 	
 	if(strcmp("join",command) == 0)
 	{
-		if(size_input == 2)
+		if(size_input == 3)
 		{
-			if(checkDigit(second) == 1)
+			if(checkDigit(second) == 1 && checkDigit(id) == 1)
 			{
 				*instr_code = JOIN_ID;
 				
 				return getParam(terminal);
 			}
 		}
-		if(size_input == 4)
+		else if(size_input == 5)
 		{
-			if(checkDigit(second) == 1 && isPort(bootTCP) == 1)
+			if(checkDigit(second) == 1 && checkDigit(id) == 1 && isIP(bootIP) == 1 && isPort(bootTCP) == 1)
 			{
 				*instr_code = JOIN_LINK;
 				return getParam(terminal);
@@ -79,7 +67,8 @@ char *readCommand(enum instr *instr_code)
 		}
 		else
 		{
-			printf("Wrong format! Commands: join id, join id bootIP bootTCP\n");
+			printf("Wrong format! Commands: join net id, join net id bootIP bootTCP\n");
+			return NULL;
 		}	
 	}
 	else if(strcmp("create",command) == 0)
@@ -93,6 +82,7 @@ char *readCommand(enum instr *instr_code)
 		else
 		{
 			printf("Wrong format! Command: create subname\n");
+			return NULL;
 		}
 	}
 	else if(strcmp("get",command) == 0)
@@ -106,39 +96,43 @@ char *readCommand(enum instr *instr_code)
 		else
 		{
 			printf("Wrong format! Command: get name\n");
+			return NULL;
 		}
 	}
-	else if(strcmp("show topology",command) == 0 || strcmp("st",command) == 0)
+	else if((strcmp("show",command) == 0 && strcmp("topology",second) == 0) || strcmp("st",command) == 0)
 	{
-		if(size_input == 1)
+		if((size_input == 1 && strcmp("st",command) == 0) || (size_input == 2 && strcmp("show",command) == 0 && strcmp("topology",second) == 0))
 		{
 			*instr_code = ST;
 		}
 		else
 		{
 			printf("Wrong format in command! Command: show topology or st\n");
+			return NULL;
 		}
 	}
-	else if(strcmp("show routing",command) == 0 || strcmp("sr",command) == 0)
+	else if((strcmp("show",command) == 0 && strcmp("routing",second) == 0) || strcmp("sr",command) == 0)
 	{
-		if(size_input == 1)
+		if((size_input == 1 && strcmp("st",command) == 0) || (size_input == 2 && strcmp("show",command) == 0 && strcmp("routing",second) == 0))
 		{
 			*instr_code = SR;
 		}
 		else
 		{
 			printf("Wrong format in command! Command: show routing or sr\n");
+			return NULL;
 		}
 	}
-	else if(strcmp("show cache",command) == 0 || strcmp("sc",command) == 0)
+	else if((strcmp("show",command) == 0 && strcmp("cache",second) == 0) || strcmp("sc",command) == 0)
 	{
-		if(size_input == 1)
+		if((size_input == 1 && strcmp("sc",command) == 0) || (size_input == 2 && strcmp("show",command) == 0 && strcmp("cache",second) == 0))
 		{
 			*instr_code = SC;
 		}
 		else
 		{
 			printf("Wrong format in command! Command: show cache or sc\n");
+			return NULL;
 		}
 	}
 	else if(strcmp("leave",command) == 0)
@@ -150,6 +144,7 @@ char *readCommand(enum instr *instr_code)
 		else
 		{
 			printf("Wrong format in command! Command: leave\n");
+			return NULL;
 		}
 	}
 	else if(strcmp("exit",command) == 0)
@@ -161,14 +156,15 @@ char *readCommand(enum instr *instr_code)
 		else
 		{
 			printf("Wrong format in command! Command: exit\n");
+			return NULL;
 		}
 	}
 	
 	if(*instr_code == ERR)
 	{
 		printf("Error in command input!\n");
-		return NULL;
 	}
+		return NULL;
 }
 
 int checkDigit(char word[])
@@ -183,6 +179,45 @@ int checkDigit(char word[])
 	  return 0;
         }
   return 1;
+}
+
+int isIP(char ip[]) 
+{ 
+   int num, dots = 0;
+   char *ptr;
+   
+   if (ip == NULL)
+   return 0;
+      
+   ptr = strtok(ip, "."); //cut the string using dor delimiter
+   if (ptr == NULL)
+   return 0;
+   
+   while (ptr) 
+   {
+      if (!checkDigit(ptr)) //check whether the sub string is holding a number or not
+      {   
+         return 0;
+      }  
+         num = atoi(ptr); //convert substring to number
+         
+      if (num >= 0 && num <= 255) 
+      {
+         ptr = strtok(NULL, "."); //cut the next part of the string
+         if (ptr != NULL)
+         dots++; //increase the dot count
+      } 
+      else
+      {
+           return 0;
+	  }
+   }
+    
+   if (dots != 3) //if the number of dots are not 3, return false
+   {   
+       return 0;
+   }
+      return 1;
 }
 
 int isPort(char port[])
